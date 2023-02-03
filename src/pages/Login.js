591f6d339090fcccb98aead28d5510d0e043a3db
Matheus <maahwiski@gmail.com>
2023-02-03 19:43:03 -0300

    finaliza requisito 1 e 2

---
+++ src/pages/Login.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  state = {
+    nameArea: '',
+    isSaveButtonDisabled: true,
+    loading: false,
+  };
+
+  async handleClick() {
+    const { nameArea } = this.state;
+    const { history } = this.props;
+
+    this.setState({ loading: true });
+
+    await createUser({ name: nameArea });
+
+    this.setState({ loading: false });
+
+    history.push('/search');
+  }
+
+  validateInputs = () => {
+    const { nameArea } = this.state;
+
+    const minLength = 3;
+    const validateName = nameArea.length < minLength;
+
+    this.setState({ isSaveButtonDisabled: validateName });
+  };
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    this.setState({ [name]: target.value }, () => this.validateInputs());
+  };
+
+  render() {
+    const { isSaveButtonDisabled, loading, nameArea } = this.state;
+
+    if (loading) return <Loading />;
+
+    return (
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="name-area">
+            <input
+              type="text"
+              name="nameArea"
+              placeholder="insira seu nome"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+              value={ nameArea }
+            />
+
+            <input
+              type="button"
+              name="isSaveButtonDisabled"
+              data-testid="login-submit-button"
+              value="Entrar"
+              disabled={ isSaveButtonDisabled }
+              onClick={ this.handleClick }
+            />
+          </label>
+
+        </form>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.func.isRequired,
+};
+
+export default Login;
