9b5eb8692f7532a040733d3ab0767facfc36ee57
Matheus <maahwiski@gmail.com>
2023-02-13 18:11:31 -0300

    finaliza requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
+  state = {
+    loading: false,
+    checked: false,
+  };
+
+  handleChange = async () => {
+    const { music } = this.props;
+    this.setState({
+      loading: true,
+    });
+    await addSong(music);
+    this.setState({
+      loading: false,
+      checked: true,
+    });
+  };
+
   render() {
     const { music } = this.props;
-    const { trackNumber, trackName, previewUrl } = music;
+    const { trackNumber, trackName, previewUrl, trackId } = music;
+    const { loading, checked } = this.state;
     return (
       <main>
         <div key={ trackNumber }>
           <h3>{trackName}</h3>
         </div>
+
+        <label htmlFor={ trackId } data-testid={ `checkbox-music-${trackId}` }>
+          Favorita
+          <input
+            type="checkbox"
+            id={ trackId }
+            checked={ checked }
+            onChange={ this.handleChange }
+          />
+        </label>
+        { loading && <Loading />}
+
         <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
           O seu navegador não suporta o elemento
